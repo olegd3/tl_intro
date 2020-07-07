b669bb60d74c36442ec8f98df8019e99f8e02139
tl <dudelzak@gmail.com>
2020-07-07 12:51:10 +0700

    0707 mid

--- tl/detection_01.py
+++ tl/detection_01.py
@@ -2,21 +2,25 @@ from imageai.Detection import ObjectDetection
 import os
 import config_detection
 
-#  SOURCE_PATH = "/home/tl/images/kholmy/200626_kholmy"
+# SOURCE_PATH = "/home/tl/images/kholmy/200626_kholmy"
 
 execution_path = os.getcwd()
 source_images = config_detection.SOURCE_PATH
 print(source_images)
 files = os.listdir(source_images)
-print(f'Execution_path is  {execution_path}')
-print(f'Test image is {files[300]}')
 detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(execution_path , "yolo.h5"))
+
+# detector.setModelTypeAsYOLOv3()
+# detector.setModelPath(os.path.join(execution_path , "yolo.h5"))
+
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+
+
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image='/home/tl/tl_intro/tl/358905.jpeg', 
-                                             output_image_path='/home/tl/tl_intro/tl/imagenew.jpeg', 
-                                             minimum_percentage_probability=30)
+detections = detector.detectObjectsFromImage(input_image=os.path.join(source_images, files[302]), 
+                                             output_image_path='/home/tl/tl_intro/tl/imagenew5.jpeg', 
+                                             minimum_percentage_probability=50)
 
 for eachObject in detections:
     print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
